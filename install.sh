120f159e1ccaf76b75734f260402b7ce66499b1d
Frank Chiarulli Jr <frank@frankchiarulli.com>
2018-10-24 12:28:03 -0400

    removes windows automated installing

--- install.sh
+++ install.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-windowsUsername="fcjr"
-
-# ask if user wants oh-my-zsh installed
-read -p "Install Oh-my-zsh (y/N)? " choice
-if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-    echo "Installing Oh-my"
-    #install zsh & curl
-    apt-get install zsh curl
-
-    #change shell to zsh
-    chsh -s $(which zsh)
-
-    #install oh-my-zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-#create vim temp directory
 mkdir ~/.vimtmpdir
 
 # for over all standard dotfiles
@@ -28,33 +11,4 @@ for file in $(find standard/ -name ".*" -type f -printf "%f\n" ); do
     fi
     # symlink file
     ln -s $PWD/standard/$file ~/$file
-
-    # WSL Specific Configuration
-    # if we are running in WSL move the generic files into windows root as well
-    if grep -q Microsoft /proc/sys/kernel/osrelease; then
-        if [ -e /mnt/c/Users/$windowsUsername/$file ]; then
-            mv -f /mnt/c/Users/$windowsUsername/$file{,.old}
-            # symlink file
-            ln -s $PWD/standard/$file /mnt/c/Users/$windowsUsername/$file
-        fi
-    fi
-
 done
-
-#WSL Specific Configuration
-if grep -q Microsoft /proc/sys/kernel/osrelease; then
-    # update VS Code settings
-    if [ -e /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json ]; then
-        mv -f /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json{,.old}
-        ln -s $PWD/other/vscode_settings.json /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json
-    fi
-    #update wsl.conf and link windows ssl keys to wsl
-    if [ -e /etc/wsl.conf ]; then
-        mv -f /etc/wsl.conf{,.old}
-        ln -s $PWD/other/wsl.conf /etc/wsl.conf
-    fi
-    if [ -e ~/.ssh ]
-        mv -f ~/.ssh{,.old}
-        ln -s /mnt/c/User/$windowsUsername/.ssh ~/.ssh
-    fi
-fi
--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 windowsUsername="fcjr"
 
 #for over each dotfile
-for file in $(find generic/ -name ".*" -type f -printf "%f\n" ); do
+for file in $(find standard/ -name ".*" -type f -printf "%f\n" ); do
     # if the file is a symlinked, remove symlink
     if [ -h ~/$file ]; then
         rm -f ~/$file
@@ -12,39 +12,4 @@ for file in $(find generic/ -name ".*" -type f -printf "%f\n" ); do
     if [ -e ~/${file}.old ]; then
         mv -f ~/$file{.old,}
     fi
-
-    # remove windows symlinks if in wsl
-    if grep -q Microsoft /proc/sys/kernel/osrelease; then
-        if [ -h /mnt/c/Users/$windowsUsername/$file ]; then
-            rm -f /mnt/c/Users/$windowsUsername/$file
-        fi
-        #if .old file exists restore it
-        if [ -e /mnt/c/Users/$windowsUsername/$file ]; then
-            mv -f /mnt/c/Users/$windowsUsername/$file{.old,}
-        fi
-    fi
 done
-
-#WSL Specific Configuration
-if grep -q Microsoft /proc/sys/kernel/osrelease; then
-    # remove VS Code settings
-    if [ -h /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json ]; then
-        rm -f /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json
-    fi
-    if [ -e /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json.old ]; then
-        mv -f /mnt/c/Users/$windowsUsername/AppData/Roaming/Code/User/settings.json{.old,}
-    fi
-    #remove wsl.conf and unlink windows ssl keys to wsl
-    if [ -h /etc/wsl.conf ]; then
-        rm -f /etc/wsl.conf
-    fi
-    if [ -e /etc/wsl.conf.old ]; then
-        mv -f /etc/wsl.conf{.old,}
-    fi
-    if [ -h ~/.ssh ]; then
-        rm -f ~/.ssh
-    fi
-    if [ -e ~/.ssh.old]; then
-        mv ~/.ssh{.old,}
-    fi
-fi
