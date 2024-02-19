02ef639886e8f03a3a357b19ee370cf7a969b3c6
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2024-02-19 17:58:52 -0500

    setup wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,4 +4,6 @@ local window_frame = require('lua/rose-pine').window_frame()
 return {
     colors = colors,
     window_frame = window_frame,
+    use_fancy_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
 }
