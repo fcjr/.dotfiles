a11006e4a566265202df2c8df2f9284703436347
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2024-02-19 18:31:27 -0500

    dont ask for confirmation when closing windows

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local window_frame = require('lua/rose-pine').window_frame()
 return {
     colors = colors,
     window_frame = window_frame,
+    window_close_confirmation = 'NeverPrompt',
     use_fancy_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
 }
