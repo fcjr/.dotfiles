2cad163ff3eea775d846637231864419a59ab156
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2024-02-19 18:54:41 -0500

    setup fonts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,12 @@
+local wezterm = require('wezterm')
 local colors = require('lua/rose-pine').colors()
 local window_frame = require('lua/rose-pine').window_frame()
 
 return {
     colors = colors,
     font_size = 13.0,
+    font = wezterm.font('MesloLGSDZ Nerd Font'),
+    freetype_load_flags = 'NO_HINTING',
     window_frame = window_frame,
     window_close_confirmation = 'NeverPrompt',
     use_fancy_tab_bar = true,
